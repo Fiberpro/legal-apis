4a63dff6e0680837d047e3528b1db80dfb6d64a8
Fiberpro <139153095+Fiberpro@users.noreply.github.com>
2026-08-21 09:31:09 -0500

    Add optional SendGrid API key and from email
    Added optional fields for SendGrid API key and from email.

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,14 @@
+from typing import Optional
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     ODOO_URL: str
     ODOO_DB: str
     ODOO_USER: str
-    ODOO_PASSWORD: str 
+    ODOO_PASSWORD: str
+
+    SENDGRID_API_KEY: Optional[str] = None
+    FROM_EMAIL: Optional[str] = None
 
     model_config = SettingsConfigDict(env_file=".env",extra="ignore", env_file_encoding="utf-8")
 
